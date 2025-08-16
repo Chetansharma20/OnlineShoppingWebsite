3e4bd9a231e2cea4000db2d32d0ea75b23bdb86f
Chetansharma20 <sharmachetan20082000@gmail.com>
2025-08-16 19:41:59 +0530

    Clean repo

---
+++ server/src/Controllers/PassporrtController.js
@@ -0,0 +1,19 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // attach user object or save to DB
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
